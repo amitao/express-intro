f94485cba7cfc1f713449dfcd49a444d58e5ae41
Amie Thao <amie.c.thao@gmail.com>
2018-11-13 11:21:46 -0600

    add express js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+let express = require('express');
+
+let app = express();
+let PORT = 5000;
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, function(){
+  console.log(`listening to port ${PORT}`);
+});
